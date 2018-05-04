a3e8baf2d046a1736c075bb89da1e547e963be38
Slava Vishnyakov <bomboze@gmail.com>
2018-05-04 14:46:18 +0300

    Create DefaultDict.php

---
+++ DefaultDict.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ChromeDevtoolsProtocol;
+
+class DefaultDict {
+
+    private $defaultValue;
+
+    private $array = [];
+
+    public function __construct($defaultValue)
+    {
+        $this->defaultValue = $defaultValue;
+    }
+
+    public function has($key)
+    {
+        return array_key_exists($key, $this->array);
+    }
+
+    public function get($key)
+    {
+        if(!$this->has($key)) {
+            $this->array[$key] = $this->defaultValue;
+        }
+        return $this->array[$key];
+    }
+
+    public function increment($key)
+    {
+        return $this->set($key, $this->get($key) + 1);
+    }
+
+    public function pushToKey($key, $value)
+    {
+        $this->array[$key] []= $value;
+    }
+
+	public function shiftFirstFromKey($key)
+	{
+		return array_shift($this->array[$key]);
+	}
+
+	public function decrement($key)
+	{
+		return $this->set($key, $this->get($key) - 1);
+	}
+
+	public function set($key, $value)
+	{
+		$this->array[$key] = $value;
+		return $value;
+	}
+
+	public function countOfKey($key)
+	{
+		return count($this->array[$key]);
+	}
+
+
+}
